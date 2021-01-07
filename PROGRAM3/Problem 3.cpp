4d1e13ed0d48767bb9f3d05bca39ce87ce522b3d
blink616 <51245088+blink616@users.noreply.github.com>
2021-01-07 18:31:32 +0500

    Problem 3 initial

--- PROGRAM3/Problem 3.cpp
+++ PROGRAM3/Problem 3.cpp
@@ -1,88 +1,56 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-// Function to find the minimum cost
-// to convert string a to b
-void convertString(string a, string b)
+
+int recursiveFunction(string str1, string str2, int m, int n)
 {
-    \
-    vector<char> char_a, char_b;
+    int L[m + 1][n + 1];
+    int i, j;
 
-    //store characters in a vector
-    for (char c : a)
+    for (i = 0; i <= m; i++)
     {
-        
-        char_a.push_back(c);
-        cout<<"String a: "<<c<<endl;
-    
-    }
-
-    for (char c : b)
-    {
-        
-        char_b.push_back(c);
-        cout<<"String b: "<<c<<endl;
-    }
-
-    int size;
-    
-
-    //find the smallest string
-    size = min(char_a.size(), char_b.size());
-    
-
-    //insert characters
-    for (int i = 0; i < size; ++i)
-    {
-        if (char_a[i] != char_b[i])
+        for (j = 0; j <= n; j++)
         {
-            cout << "Inserted " << char_b[i] << endl;
-            char_b[i] = char_a[i];
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+
+            else if (str1.at(i - 1) == str2.at(j - 1))
+                L[i][j] = L[i - 1][j - 1] + 1;
+
+            else
+            {
+                
+                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+            }
         }
     }
 
-
-    //if string to be converted is greater remove characters
-    if (char_b.size() > char_a.size())
-    {
-        char removed;
-        int i = char_b.size();
-        while (i > char_a.size()){
-            removed = char_b.back();
-        char_b.pop_back();
-        cout << "Deleted " << removed << " from String b " <<endl;
-        i--;
-        }
-    }
-//if string to be converted is smaller add characters
-    else
-    {
-        int i = char_b.size();
-        char added;
-        while (i < char_a.size())
-        {
-            added = char_a[i];
-            char_b.push_back(added);
-            cout << "Inserted " << char_a[i] << " at the end of the String b " << endl;
-            i++;
-        }
-    }
-    cout<<"Converted String: "<<endl;
-    for (char c : char_b)
-        cout<<c;
+    return L[m][n];
 }
 
-// Driver Code
+void printMinDelAndInsert(string str1, string str2)
+{
+    int m = str1.size();
+    int n = str2.size();
+
+    int len = recursiveFunction(str1, str2, m, n);
+    cout << "Minimum number of deletions = " << (m - len)
+         << endl;
+
+    cout << "Minimum number of insertions = " << (n - len)
+         << endl;
+}
+
+
 int main()
 {
-    string a, b;
-    cout << "Enter the string to be converted: " << endl;
-    getline(cin, b);
-    cout << "Enter the string to be matched: " << endl;
-    getline(cin, a);
-
-    // Function Call
-    convertString(a, b);
-
+    string str1, str2;
+    cout << "Enter first string: " << endl;
+    getline(cin, str1);
+    cout << "Enter second string: " << endl;
+    getline(cin, str2);
+    
+    printMinDelAndInsert(str1, str2);
     return 0;
 }
