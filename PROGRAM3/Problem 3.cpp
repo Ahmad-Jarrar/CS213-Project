ae5d8bfc449dfbf47b26a056ac98a319b0b99d8a
blink616 <51245088+blink616@users.noreply.github.com>
2021-01-08 13:46:41 +0500

    Problem 3 finalized

--- PROGRAM3/Problem 3.cpp
+++ PROGRAM3/Problem 3.cpp
@@ -1,56 +1,73 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-
-int recursiveFunction(string str1, string str2, int m, int n)
+// Function to find the minimum cost
+// to convert string a to b
+void convertString(string a, string b)
 {
-    int L[m + 1][n + 1];
-    int i, j;
 
-    for (i = 0; i <= m; i++)
+    int count = 0;
+    //vector<char> char_a, char_b;
+    int freq_a[256] = {0}, freq_b[256] = {0};
+    int delete_count = 0;
+    int insert_count = 0;
+
+    //store characters in a vector
+    for (char c : a)
     {
-        for (j = 0; j <= n; j++)
+        //char_a.push_back(c);
+        freq_a[(int)c]++;
+    }
+
+    for (char c : b)
+    {
+
+        // /    char_b.push_back(c);
+        freq_b[(int)c]++;
+    }
+
+    for (int i = 0; i < b.length(); i++)
+    {
+
+        if (freq_a[(int)b.at(i)] < freq_b[(int)b.at(i)])
         {
-            if (i == 0 || j == 0)
-                L[i][j] = 0;
-
-            else if (str1.at(i - 1) == str2.at(j - 1))
-                L[i][j] = L[i - 1][j - 1] + 1;
-
+            if (b.at(i) == ' ')
+                cout << "Deleted Space" << endl;
             else
-            {
-                
-                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
-            }
+                cout << "Deleted " << b.at(i) << endl;
+
+            freq_b[(int)b.at(i)]--;
+            delete_count++;
         }
     }
 
-    return L[m][n];
+    for (int i = 0; i < a.length(); i++)
+    {
+        if (freq_b[(int)a.at(i)] == 0)
+        {
+            if (a.at(i) == ' ')
+                cout << "Inserted Space" << endl;
+            else
+                cout << "Inserted " << a.at(i) << endl;
+            insert_count++;
+        }
+    }
+
+    cout << "Minimum Number of deletions: " << delete_count << endl;
+    cout << "Minimum Number of insertions: " << insert_count << endl;
 }
 
-void printMinDelAndInsert(string str1, string str2)
-{
-    int m = str1.size();
-    int n = str2.size();
-
-    int len = recursiveFunction(str1, str2, m, n);
-    cout << "Minimum number of deletions = " << (m - len)
-         << endl;
-
-    cout << "Minimum number of insertions = " << (n - len)
-         << endl;
-}
-
-
+// Driver Code
 int main()
 {
-    string str1, str2;
-    cout << "Enter first string: " << endl;
-    getline(cin, str1);
-    cout << "Enter second string: " << endl;
-    getline(cin, str2);
-    
-    printMinDelAndInsert(str1, str2);
+    string a, b;
+    cout << "Enter the string to be converted: " << endl;
+    getline(cin, b);
+    cout << "Enter the string to be matched: " << endl;
+    getline(cin, a);
+
+    // Function Call
+    convertString(a, b);
+
     return 0;
 }
