6a045eec07eff90bd5b755132d3a5ca0760b5256
blink616 <51245088+blink616@users.noreply.github.com>
2021-01-08 23:36:28 +0500

    Problem 3 final

--- PROGRAM3/Problem 3.cpp
+++ PROGRAM3/Problem 3.cpp
@@ -7,7 +7,7 @@ void convertString(string a, string b)
 {
 
     int count = 0;
-    //vector<char> char_a, char_b;
+
     int freq_a[256] = {0}, freq_b[256] = {0};
     int delete_count = 0;
     int insert_count = 0;
@@ -15,44 +15,65 @@ void convertString(string a, string b)
     //store characters in a vector
     for (char c : a)
     {
-        //char_a.push_back(c);
         freq_a[(int)c]++;
     }
 
     for (char c : b)
     {
-
-        // /    char_b.push_back(c);
         freq_b[(int)c]++;
     }
 
+    int j = 0;
+    string b_temp = "";
     for (int i = 0; i < b.length(); i++)
     {
-
         if (freq_a[(int)b.at(i)] < freq_b[(int)b.at(i)])
         {
+
             if (b.at(i) == ' ')
                 cout << "Deleted Space" << endl;
             else
                 cout << "Deleted " << b.at(i) << endl;
-
             freq_b[(int)b.at(i)]--;
             delete_count++;
         }
-    }
-
-    for (int i = 0; i < a.length(); i++)
-    {
-        if (freq_b[(int)a.at(i)] == 0)
+        else
         {
-            if (a.at(i) == ' ')
-                cout << "Inserted Space" << endl;
-            else
-                cout << "Inserted " << a.at(i) << endl;
-            insert_count++;
+            string temp1(1, b.at(i));
+            b_temp += temp1;
         }
     }
 
+    b = b_temp;
+
+    for (int i = 0; i < a.length(); i++)
+    {
+
+        if (i < b.length() && a.at(i) == b.at(i))
+        {
+
+            continue;
+        }
+        else
+        {
+
+            string temp2(1, a.at(i));
+
+            if (i < b.length())
+            {
+                b.insert(i, temp2);
+            }
+            else
+                b += temp2;
+
+            if (temp2 == " ")
+                cout << "Inserted space" << endl;
+
+            else
+                cout << "Inserted: " << a.at(i) << endl;
+            insert_count++;
+        }
+    }
     cout << "Minimum Number of deletions: " << delete_count << endl;
     cout << "Minimum Number of insertions: " << insert_count << endl;
 }
@@ -67,6 +88,7 @@ int main()
     getline(cin, a);
 
     // Function Call
+
     convertString(a, b);
 
     return 0;
