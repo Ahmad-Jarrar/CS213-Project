abc4a64957810df91adc1c2fff7c66c05bbee664
Ahmad Jarrar <ahmad.jarrar1@outlook.com>
2021-01-14 23:53:54 +0500

    Basic functionality complete

--- PROGRAM5/Board.py
+++ PROGRAM5/Board.py
@@ -15,8 +15,9 @@ class Board(ABC):
         self.cell_size = cell_size
         self.border_size = border_size
         self.surface = pygame.Surface((size * cell_size, size * cell_size))
+        self.ships_hidden = False
 
-    def draw(self, screen, position):
+    def draw(self, screen, position, hide_ships=False):
         self.surface.fill(pygame.Color((43,118,187)))
         for y in range(self.size):
             for x in range(self.size):
@@ -25,9 +26,23 @@ class Board(ABC):
                                       y * self.cell_size + self.border_size,
                                       self.cell_size - 2*self.border_size, 
                                       self.cell_size - 2*self.border_size])
+        if not self.ships_hidden:
+            for ship in self.ships_list:
+                ship.draw(self.surface)
 
-        for ship in self.ships_list:
-            ship.draw(self.surface)
+        for x,y in self.misses_list:
+            pygame.draw.rect(self.surface, pygame.Color(((8, 73, 119))),
+                                     [x * self.cell_size + self.border_size,
+                                      y * self.cell_size + self.border_size,
+                                      self.cell_size - 2*self.border_size, 
+                                      self.cell_size - 2*self.border_size])
+        
+        for x,y in self.hits_list:
+            pygame.draw.rect(self.surface, pygame.Color(((255, 0, 0))),
+                                     [x * self.cell_size + self.border_size,
+                                      y * self.cell_size + self.border_size,
+                                      self.cell_size - 2*self.border_size, 
+                                      self.cell_size - 2*self.border_size])
 
         screen.blit(self.surface, position)
 
@@ -48,20 +63,6 @@ class Board(ABC):
             return True
         else:
             return False
-    
-    def get_input(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.display.quit()
-                pygame.quit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = pygame.mouse.get_pos()
-                x = x % (self.size * self.cell_size)
-                x = x // self.cell_size
-                y = y // self.cell_size
-                if x in range(self.size) and y in range(self.size):
-                    return x, y
-        return None, None
 
     def remove_ship(self, ship):
         """Removes a ship from the board"""
@@ -75,25 +76,60 @@ class Board(ABC):
                     return True
         return False
 
+    def valid_target(self, x, y):
+        """Checks whether a set of coordinates is a valid shot
+        Coordinates are within the board, and shot hasn't previously been taken
+        """
+        if x not in range(self.size) or y not in range(self.size):
+            return False
+        for previous_shot in self.misses_list + self.hits_list:
+            if (x, y) == previous_shot:
+                return False
+        return True
+
+    def shoot(self, x, y):
+        """Registers a shot on the board, saving to appropriate list"""
+        if not self.valid_target(x, y):
+            return None
+
+        for ship in self.ships_list:
+            for ship_coordinate in ship.coordinate_list:
+                if (x, y) == ship_coordinate:
+                    self.hits_list.append((x, y))
+                    return 2
+
+        self.misses_list.append((x, y))
+        return 1
+
     def get_ship(self, x, y):
         """Gets a ship object from coordinates"""
         for ship in self.ships_list:
             if (x, y) in ship.coordinate_list:
                 return ship
         return None
+    
+    @property
+    def gameover(self):
+        """Checks to see if all the ships have been fully hit"""
+        for ship in self.ships_list:
+            for coordinate in ship.coordinate_list:
+                if coordinate not in self.hits_list:
+                    return False
+        return True
 
 class PlayerBoard(Board):
     """A Board for user input"""
 
-    def __init__(self, screen, board_size, ship_sizes, cell_size, border_size):
+    def __init__(self, board_size, ship_sizes, cell_size, border_size):
         """Initialises the board by placing ships."""
         super().__init__(board_size, ship_sizes, cell_size, border_size)
-        self.display = screen
-        self.position = [board_size*cell_size, 0]
 
+        
+
+    def populate(self, screen, position, get_coordinates):
         direction = Ship.directions["NORTH"]
         while True:
-            self.draw(self.display, self.position)
+            self.draw(screen, position)
 
             # if self.ship_to_place:
             #     text = 'Click where you want your {}-long ship to be:'.format(
@@ -102,7 +138,7 @@ class PlayerBoard(Board):
             #     text = 'Click again to rotate a ship, or elsewhere if ready.'
             # self.display.show_text(text, lower=True)
 
-            x, y = self.get_input()
+            x, y = get_coordinates()
             if x is not None and y is not None:
                 ship = self.get_ship(x, y)
                 if ship:
@@ -111,7 +147,7 @@ class PlayerBoard(Board):
                     if self.is_valid(ship):
                         self.add_ship(ship)
                 elif self.ship_to_place:
-                    ship = Ship(x, y, direction, self.ship_to_place, cell_size)
+                    ship = Ship(x, y, direction, self.ship_to_place, self.cell_size)
                     if self.is_valid(ship):
                         self.add_ship(ship)
                     else:
@@ -142,12 +178,13 @@ class AIBoard(Board):
     def __init__(self, board_size, ship_sizes, cell_size, border_size):
         """Initialises the board by randomly placing ships"""
         super().__init__(board_size, ship_sizes, cell_size, border_size)
+        self.ships_hidden = True
         for ship_length in self.ship_sizes:
             ship_added = False
             while not ship_added:
                 x = random.randint(0, board_size - 1)
                 y = random.randint(0, board_size - 1)
-                ship_direction = random.choice(Ship.directions.values)
+                ship_direction = random.randint(0,3)
                 ship = Ship(x, y, ship_direction, ship_length, cell_size)
                 if self.is_valid(ship):
                     self.add_ship(ship)
--- PROGRAM5/game.py
+++ PROGRAM5/game.py
@@ -1,33 +1,67 @@
 import pygame
 import random
-from abc import ABC
-from enum import Enum
-from Board import PlayerBoard
-from itertools import zip_longest
+from Board import PlayerBoard, AIBoard
 
 class Game():
-    def __init__(self, board_size=8, cell_size=40, margin=1, ship_sizes=[4, 3, 3, 2, 2, 2]):
+    def __init__(self, board_size=8, cell_size=40, margin=20, ship_sizes=[4, 3, 3, 2, 2, 2]):
         pygame.init()
         self.board_size = board_size
         self.cell_size = cell_size
         self.margin = margin
         self.ship_sizes = ship_sizes
-        self.window_size = [2 * board_size * cell_size + margin, board_size * cell_size + margin + 200]
+
+        self.subwindow_positions = {
+            "AI Board": [self.margin, self.margin],
+            "Player Board": [2*self.margin + board_size*cell_size, self.margin],
+            "Menu": [self.margin, 2*self.margin + board_size*cell_size]
+        }
+
+
+        self.window_size = [2 * board_size * cell_size + 3*margin, board_size * cell_size + 2*margin + 200]
         self.screen = pygame.display.set_mode(self.window_size)
         pygame.display.set_caption("Battleships")
         self.screen.fill(pygame.Color(236,240,241))
 
-        self.pboard = PlayerBoard(self.screen, board_size, ship_sizes, cell_size, 0.3)
+        self.pboard = PlayerBoard(board_size, ship_sizes, cell_size, 0.3)
+        self.pboard.populate(self.screen, self.subwindow_positions["Player Board"], self.get_input)
+
+        self.aiboard = AIBoard(board_size, ship_sizes, cell_size, 0.3)
 
     def play(self):
         while not self.game_over:
-            self.get_input()
+            if self.player_turn():
+                self.ai_turn()
+                
             self.screen.fill(pygame.Color(236,240,241))
-            # self.board.draw(self.screen)
+            self.aiboard.draw(self.screen, self.subwindow_positions["AI Board"])
+            self.pboard.draw(self.screen, self.subwindow_positions["Player Board"])
+            
             pygame.display.flip()
             pygame.time.Clock().tick(60)
-            print("everything ok")
+            
+    def player_turn(self):
+        """Uses input to decide if a shot is valid or not"""
+        x, y = self.get_input()
+        if self.aiboard.valid_target(x, y):
+            result = self.aiboard.shoot(x, y)
+            # animate
+            return True
+        else:
+            return False
 
+    def ai_turn(self):
+        """Uses input to decide if a shot is valid or not"""
+        x, y = -1, -1
+        while not self.pboard.valid_target(x, y):
+            x = random.randint(0, self.board_size - 1)
+            y = random.randint(0, self.board_size - 1)
+        
+        if self.aiboard.valid_target(x, y):
+            result = self.pboard.shoot(x, y)
+            # animate
+            return True
+        else:
+            return False
 
     def get_input(self):
         for event in pygame.event.get():
@@ -46,7 +80,15 @@ class Game():
     # TODO: Game over logic
     @property
     def game_over(self):
-        return False
+        """Determines and prints the winner"""
+        if self.aiboard.gameover:
+            print("Congratulations you won")
+            return True
+        elif self.pboard.gameover:
+            print("Congratulations you lost")
+            return True
+        else:
+            return False
 
     
 
