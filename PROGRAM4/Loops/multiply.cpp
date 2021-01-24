bd3e0fb79b48127e79d1434894ab514d96d9a0c0
Ahmad Jarrar <ahmad.jarrar1@outlook.com>
2021-01-24 13:40:26 +0500

    Name Change | Bug fixes

--- PROGRAM4/Loops/multiply.cpp
+++ PROGRAM4/Loops/multiply.cpp
@@ -4,7 +4,7 @@
 
 using namespace boost::numeric::ublas;
 
-matrix<int> matmul_naive (matrix<int> m, matrix<int> n) {
+matrix<int> matmul_loops (matrix<int> m, matrix<int> n) {
 
     if (m.size2() != n.size1()) {
         perror("m.size2() != n.size1()\n");
@@ -13,25 +13,17 @@ matrix<int> matmul_naive (matrix<int> m, matrix<int> n) {
     }
     matrix<int> mn(m.size1(), n.size2());
 
-    // No need. Matrix is automatically initialized to zero
-    // initialize_matrix(mn, false, 0);
+    initialize_matrix(&mn, false, 0);
 
     for (size_t i = 0; i < m.size1(); i++)
-    {
         for (size_t j = 0; j < n.size2(); j++)
-        {
             for (size_t k = 0; k < m.size2(); k++)
-            {
                 mn(i,j) += m(i,k) * n(k,j);
-            }
-            
-        }
-        
-    }
+
     return mn;
 }
 // https://gist.github.com/nadavrot/5b35d44e8ba3dd718e595e40184d03f0
-matrix<int> matmul_naive_optimized (matrix<int> m, matrix<int> n) {
+matrix<int> matmul_loops_optimized (matrix<int> m, matrix<int> n) {
 
     if (m.size2() != n.size1()) {
         perror("m.size2() != n.size1()\n");
@@ -40,31 +32,22 @@ matrix<int> matmul_naive_optimized (matrix<int> m, matrix<int> n) {
     }
     matrix<int> mn(m.size1(), n.size2());
 
-    // No need. Matrix is automatically initialized to zero
-    // initialize_matrix(mn, false, 0);
+    initialize_matrix(&mn, false, 0);
 
     for (size_t i = 0; i < m.size1(); i++)
-    {
         for (size_t k = 0; k < m.size2(); k++)
-        {
             for (size_t j = 0; j < n.size2(); j++)
-            {
                 mn(i,j) += m(i,k) * n(k,j);
-            }
-            
-        }
-        
-    }
     return mn;
 }
 
-void multiply_slice(matrix<int> mn, matrix<int> m, matrix<int> n, int i) {
+void multiply_slice(matrix<int> &mn, matrix<int> m, matrix<int> n, int i) {
     for (size_t k = 0; k < m.size2(); k++)
         for (size_t j = 0; j < n.size2(); j++)
             mn(i,j) += m(i,k) * n(k,j);
 }
 
-matrix<int> matmul_naive_optimized_threaded (matrix<int> m, matrix<int> n) {
+matrix<int> matmul_loops_optimized_threaded (matrix<int> m, matrix<int> n) {
 
     if (m.size2() != n.size1()) {
         perror("m.size2() != n.size1()\n");
@@ -73,12 +56,11 @@ matrix<int> matmul_naive_optimized_threaded (matrix<int> m, matrix<int> n) {
     }
     matrix<int> mn(m.size1(), n.size2());
 
-    // No need. Matrix is automatically initialized to zero
-    // initialize_matrix(mn, false, 0);
+    initialize_matrix(&mn, false, 0);
     std::thread threads[m.size1()];
     for (size_t i = 0; i < m.size1(); i++)
     {
-        threads[i] = std::thread(multiply_slice, mn, m, n, i);
+        threads[i] = std::thread(multiply_slice, std::ref(mn), m, n, i);
     }
 
     for (size_t i = 0; i < m.size1(); i++)
@@ -113,28 +95,28 @@ int main()
     // print_matrix(B);
     
     auto start = std::chrono::high_resolution_clock::now();
-    C = matmul_naive(A, B);
+    C = matmul_loops(A, B);
     auto stop = std::chrono::high_resolution_clock::now();
 
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
+    // print_matrix(C);
+    std::cout << "Time taken loops(microseconds): " << duration.count() << std::endl;
+    
+    start = std::chrono::high_resolution_clock::now();
+    C = matmul_loops_optimized(A, B);
+    stop = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Time taken Naive(microseconds): " << duration.count() << std::endl;
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
+    // print_matrix(C);
+    std::cout << "Time taken loops Optimized(microseconds): " << duration.count() << std::endl;
 
     start = std::chrono::high_resolution_clock::now();
-    C = matmul_naive_optimized(A, B);
+    C = matmul_loops_optimized_threaded(A, B);
     stop = std::chrono::high_resolution_clock::now();
 
     duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
 
-    std::cout << "Time taken Naive Optimized(microseconds): " << duration.count() << std::endl;
-
-    start = std::chrono::high_resolution_clock::now();
-    C = matmul_naive_optimized_threaded(A, B);
-    stop = std::chrono::high_resolution_clock::now();
-
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
-
-    std::cout << "Time taken Naive Optimized Threaded(microseconds): " << duration.count() << std::endl;
+    std::cout << "Time taken loops Optimized Threaded(microseconds): " << duration.count() << std::endl;
 
     // Print product
     // print_matrix(C);
