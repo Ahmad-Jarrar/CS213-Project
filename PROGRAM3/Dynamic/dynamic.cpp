803c688d3bbde632f6a9fed3ffc5c850fb90f6e6
blink616 <51245088+blink616@users.noreply.github.com>
2021-01-25 05:31:56 +0500

    Final Commit

--- PROGRAM3/Dynamic/dynamic.cpp
+++ PROGRAM3/Dynamic/dynamic.cpp
@@ -7,11 +7,13 @@ void find_characters(string str1, string str2)
 {
     int n = str1.length();
     int m = str2.length();
- 
+
+    deletions = "";
+    insertions = "";
     // declaring character array
     char str1_array[n + 1];
     char str2_array[m + 1];
- 
+
     // copying the contents of the
     // string to char array
     strcpy(str1_array, str1.c_str());
@@ -20,38 +22,45 @@ void find_characters(string str1, string str2)
     string lcs = longest_common_subseq(str1_array, str2_array, n, m);
 
     int ptr = 0;
-    
+
     // Compare lcs with first string to get the
     // characters which are to be deleted
-    for (int i = 0; i < str1.size(); i++)
+    for (register int i = 0; i < str1.size(); ++i)
     {
         if (str1[i] == lcs[ptr])
             ptr++;
         else
-            deletions.push_back(str1[i]);
+            deletions = deletions +  "\'" + str1[i] + "\' ";
     }
 
     ptr = 0;
-    
+
     // Compare lcs with second string to get the
     // characters which are to be inserted
-    for (int i = 0; i < str2.size(); i++)
+    for (register int i = 0; i < str2.size(); ++i)
     {
         if (str2[i] == lcs[ptr])
             ptr++;
         else
-            insertions.push_back(str2[i]);
+            insertions = insertions + "\'" + str2[i] + "\' ";
     }
 }
 
 int main()
 {
-    cout << endl << "Program 3: String conversion insertions and deletions.";
-    cout << endl << "Dynamic Programming Approach.";
-    cout << endl << endl << "This program takes in 2 strings, and outputs the";
-    cout << endl << "characters to be inserted and deleted for conversion!";
-    cout << endl << endl;
-    cout << "You may enter -1 at any input prompt to exit." << endl << endl;
+    cout << endl
+         << "Program 3: String conversion insertions and deletions.";
+    cout << endl
+         << "Dynamic Programming Approach.";
+    cout << endl
+         << endl
+         << "This program takes in 2 strings, and outputs the";
+    cout << endl
+         << "characters to be inserted and deleted for conversion!";
+    cout << endl
+         << endl;
+    cout << "You may enter -1 at any input prompt to exit." << endl
+         << endl;
 
     string str1, str2;
     // keeps prompting for inputs until program terminated
@@ -68,11 +77,12 @@ int main()
 
         // prints the result and empties the vectors
         print_result();
-        empty_vectors();
+        
 
         cout << "Elapsed time for Dynamic Approach: ";
         cout << chrono::duration_cast<chrono::microseconds>(end - start).count()
-            << " microseconds." << endl << endl;
+             << " microseconds." << endl
+             << endl;
     }
 
     return 0;
